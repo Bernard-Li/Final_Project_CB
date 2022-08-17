65822aada1a231c5360b38ee40f611aa13f7e399
Bernard <bernie.li@live.ca>
2022-08-17 18:53:33 -0400

    Client_Server_Comp

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+const App = () =>{
+  return (
+    <>
+      <h1>This is Swivy!</h1>
+    </>
+  )
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,2 +1,13 @@
 const express = require('express');
+const PORT = 8000;
 
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+  res.send('Server has started');
+})
+const server = app.listen(PORT, () =>{
+  console.info("🌍 Listening on port: " + server.address().port);
+});
