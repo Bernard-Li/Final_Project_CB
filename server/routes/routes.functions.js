dadc6409ee9ada905a895987170f2847305898f6
Bernard <bernie.li@live.ca>
2022-08-18 23:40:03 -0400

    uuid package added

--- server/routes/routes.functions.js
+++ server/routes/routes.functions.js
@@ -1,6 +1,9 @@
 //This folder will contain all the "handler" functions for each route
 //We will be retrieving user info from mongoDB
 
+//Using this package to generate random ID's when a new user is created.
+//Stored in mongoDB collectin: uniqueId
+const {v4: uuidv4 } = require('uuid');
 const { MongoClient } = require('mongodb');
 require("dotenv").config();
 const { MONGO_URI } = process.env;
@@ -9,7 +12,8 @@ const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }
-
-//create a function that will go to the database and grab the current user
+//create a function that will allow a user to create an account 
+const createUser = () =>{};
+//create a function that will go to the database and grab the current user? Double check with how Auth0 works
 const currentUser = () =>{};
 
