671d45969c9c652450649d4067ace032eeb82f2f
Bernard <bernie.li@live.ca>
2022-08-25 17:02:58 -0400

    Beginning setup for backend to store new google user into databse in Mongo, null values passed

--- client/src/auth0provider/login.js
+++ client/src/auth0provider/login.js
@@ -1,9 +1,22 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import React, { useEffect } from "react";
 
 const LoginButton = () => {
   const { loginWithRedirect , user} = useAuth0();
-  console.log(user);
+  
+  useEffect(() => {
+    const createNewUser = async () =>{
+      await fetch(`/create-user`,{
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+          "Content-Type" : "application/json",
+        }
+      })
+    }
+    createNewUser();
+  }, [])
+
   return <button onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
