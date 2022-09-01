edb8352be1e0775ef80027751bace51d8d69ebe9
Bernard <bernie.li@live.ca>
2022-08-31 22:09:07 -0400

    Login button CSS

--- client/src/auth0provider/login.js
+++ client/src/auth0provider/login.js
@@ -1,16 +1,26 @@
 import { useAuth0 } from "@auth0/auth0-react";
-
+import styled from "styled-components";
 //Function that contains the login button to allow a redirect with Auth0 once the user is authenticated. 
 const LoginButton = () => {
   const { loginWithRedirect , user} = useAuth0();
   //If no user has logged in, the "log in" button will be displayed. Otherwise, the logout function/button will takes its place. 
   return (
-  <>
+  <Wrapper>
   {!user &&
-  <button onClick={() => loginWithRedirect()}>Log In</button>
+  <button 
+    className="login-btn"
+    onClick={() => loginWithRedirect()}>Log In</button>
   }
-  </>
+  </Wrapper>
   )
 };
 
 export default LoginButton;
+
+const Wrapper = styled.div`
+
+.login-btn{
+  margin: 10px;
+  color: white;
+  width: 80px;
+}`
